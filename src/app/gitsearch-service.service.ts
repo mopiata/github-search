93ea6a518428d55ad7a1effc09c4fa374c55ceda
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-05-09 19:22:12 +0300

    used adapter in our service to grab user data

--- src/app/gitsearch-service.service.ts
+++ src/app/gitsearch-service.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { User } from "./user";
+import { map } from "rxjs/operators";
+import { User, UserAdapter } from "./user";
 import { Repo } from "./repo";
 import { environment } from "../environments/environment";
 
@@ -13,11 +14,18 @@ export class GitsearchServiceService {
   private searchString='mopiata';
   profSearchUrl = 'https://api.github.com/users/' + this.searchString + '?access_token=' + environment.accessKey
   
-  constructor(private http:HttpClient) { }
+  constructor(
+    private http:HttpClient,
+    private adapter: UserAdapter,
+    ) { }
 
   userRequest(searchString): Observable<User[]>{
     const url=this.profSearchUrl;
-    
+  
+    return this.http.get(url).pipe(
+      // Adapt each item in the raw data array
+      map((data:any[])=> data.map(item => this.adapter.adapt(item))),
+    );    
   }
 
   repoRequest(searchString){
