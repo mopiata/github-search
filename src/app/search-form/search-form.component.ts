c51d49827af3045229cd02191067be94bfa30d61
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-05-09 14:28:46 +0300

    validated form and form clear

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search-form',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
+
+  @ViewChild('searchForm') formValues;
+
+  submitSearch(){
+    console.log('Hi');
+    this.formValues.resetForm();
+  }
 
   constructor() { }
 
